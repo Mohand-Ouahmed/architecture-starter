ec8231c245db8480b1b5237d46d58ceb10b99a39
Mohand-Ouahmed <mohand.abellache@gmail.com>
2026-07-19 20:06:46 +0200

    Create ArchStarterApplication for Spring Boot app

---
+++ src/main/java/com/mohanda/archstarter/ArchStarterApplication.java
@@ -0,0 +1,12 @@
+package com.mohanda.archstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+  public class ArchStarterApplication {
+
+    public static void main(String[] args) {
+              SpringApplication.run(ArchStarterApplication.class, args);
+    }
+  }
